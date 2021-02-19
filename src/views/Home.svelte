f23734157a1974d5054d6425686a9d25141b24d9
Androz2091 <androz2091@gmail.com>
2021-02-19 17:24:33 +0100

    :sparkles: Homepage is working

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,29 +1,39 @@
 <script>
+import { data } from "../app/store";
+
+
 </script>
 
 <div class="statistics">
-    <div class="column left">
+    <div class="profile-card">
         <div class="profile">
-            <img class="avatar" src="https://cdn.discordapp.com/avatars/422820341791064085/b65bf3d7305b6a2be9b2ab301c8b5b92.webp" alt="Avatar" />
-            <h1>Androz2091</h1>
-        </div>
-        <div class="profile-infos">
-            <div class="profile-info">
-                <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"></path></svg>                <h3 style="display: inline-block;">
-                    ID
-                    <span>422820341791064085</span>
-                </h3>
+            <div>
+                <img class="avatar" src="https://cdn.discordapp.com/{ $data.user.avatar_hash ? 'avatars/'+ $data.user.id + '/' + $data.user.avatar_hash + '.webp' : 'embed/avatars/' + $data.user.discriminator % 5 + '.png' }" alt="Avatar" />
+                <h1>
+                    { $data.user.username }<small class="text-muted">#{ $data.user.discriminator }</small>
+                </h1>
             </div>
-            <div class="profile-info">
-                <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
-                <h3 style="display: inline-block;">
+            <div>
+                <h3>
+                    ID
+                    <span class="highlight">{ $data.user.id }</span>
+                </h3>
+                <h3>
+                    Email
+                    <span class="highlight">{ $data.user.email }</span>
+                </h3>
+                <h3>
                     Phone
-                    <span>+</span>
+                    <span class="highlight">{ $data.user.phone }</span>
+                </h3>
+                <h3>
+                    IP Address
+                    <span class="highlight">{ $data.user.ip }</span>
                 </h3>
             </div>
         </div>
     </div>
-    <div class="column right">
+    <!--<div class="column right">
         <div class="cards">
             <div class="card">
                 <h2>
@@ -47,60 +57,54 @@
         <div class="top">
             Hey
         </div>
-    </div>
+    </div> -->
 </div>
 
 <style>
-    svg {
-        height: 20px;
-        display: inline-block;
+    .highlight {
+        color: #7289DA;
+        display: block;
+        margin-top: 4px;
     }
     .statistics {
-        display: flex;
         color: white;
+        padding: 20px;
     }
+    .profile-card {
+        margin-left: auto;
+        margin-right: auto;
+        background-color: #202225;
+        width: 70%;
+        border-radius: 5px;
+    }
+
     .profile {
-        margin-top: 20px;
-        border-bottom: 1px solid white;
+        padding: 10px;
         text-align: center;
-    }
-    .profile-infos {
-        margin-left: 10px;
+
+        max-width: 1200px;
+        margin: 0 auto;
+        display: grid;
+        grid-gap: 1rem;
     }
     .avatar {
-        border-radius: 10px;
+        margin-top: 20px;
+        border-radius: 50%;
     }
-    .column {
-        float: left;
+
+    @media screen and (max-width: 600px){
+        .profile-card {
+            width: 90%;
+        }
+        .avatar {
+            height: 120px;
+        }
     }
-    .left {
-        width: 30%;
+
+    @media (min-width: 600px) {
+        .profile {
+            grid-template-columns: repeat(2, 1fr);
+        }
     }
-    .right {
-        width: 70%;
-        margin-right: 10px;
-        margin-top: 10px;
-    }
-    .cards {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-gap: 10px;
-        grid-auto-rows: minmax(100px, auto);
-    }
-    span {
-        color: #7289DA;
-    }
-    .card {
-        border-radius: 10px;
-        text-align: center;
-        padding: 12px;
-        background-color: #18191c;
-    }
-    .top {
-        width: 100%;
-        padding: 12px;
-        margin-top: 10px;
-        border-radius: 10px;
-        background-color: #18191c;
-    }
+    
 </style>
