a8d339e0e261060d46a41ec639228d9234187665
Androz2091 <androz2091@gmail.com>
2021-02-19 20:39:56 +0100

    :sparkles: Rewrite grids

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -5,32 +5,16 @@ import { data } from "../app/store";
 </script>
 
 <div class="statistics">
-    <div class="profile-card">
-        <div class="profile">
-            <div>
-                <img class="avatar" src="https://cdn.discordapp.com/{ $data.user.avatar_hash ? 'avatars/'+ $data.user.id + '/' + $data.user.avatar_hash + '.webp' : 'embed/avatars/' + $data.user.discriminator % 5 + '.png' }" alt="Avatar" />
-                <h1>
-                    { $data.user.username }<small class="text-muted">#{ $data.user.discriminator }</small>
-                </h1>
+    <div class="cards">
+        <div class="card">
+            <div class="avatar">
+                <img src="https://cdn.discordapp.com/{ $data.user.avatar_hash ? 'avatars/'+ $data.user.id + '/' + $data.user.avatar_hash + '.webp' : 'embed/avatars/' + $data.user.discriminator % 5 + '.png' }" alt="Avatar" />
             </div>
-            <div>
-                <h3>
-                    ID
-                    <span class="highlight">{ $data.user.id }</span>
-                </h3>
-                <h3>
-                    Email
-                    <span class="highlight">{ $data.user.email }</span>
-                </h3>
-                <h3>
-                    Phone
-                    <span class="highlight">{ $data.user.phone }</span>
-                </h3>
-                <h3>
-                    IP Address
-                    <span class="highlight">{ $data.user.ip }</span>
-                </h3>
-            </div>
+            <h1>
+                { $data.user.username }<small class="text-muted">#{ $data.user.discriminator }</small>
+            </h1>
+        </div>
+        <div class="card">
         </div>
     </div>
     <!--<div class="column right">
@@ -61,49 +45,38 @@ import { data } from "../app/store";
 </div>
 
 <style>
-    .highlight {
-        color: #7289DA;
-        display: block;
-        margin-top: 4px;
-    }
     .statistics {
         color: white;
         padding: 20px;
     }
-    .profile-card {
-        margin-left: auto;
-        margin-right: auto;
-        background-color: #202225;
-        width: 70%;
-        border-radius: 5px;
-    }
-
-    .profile {
-        padding: 10px;
-        text-align: center;
-
-        max-width: 1200px;
-        margin: 0 auto;
+    .cards {
         display: grid;
-        grid-gap: 1rem;
+        grid-gap: 10px;
+    }
+    .card {
+        background-color: #202225;
+        border-radius: 10px;
+        display: grid;
+        justify-content: center;
     }
     .avatar {
+        text-align: center;
+    }
+    
+    .avatar img {
         margin-top: 20px;
         border-radius: 50%;
     }
 
     @media screen and (max-width: 600px){
-        .profile-card {
-            width: 90%;
-        }
-        .avatar {
+        .avatar img {
             height: 120px;
         }
     }
 
     @media (min-width: 600px) {
-        .profile {
-            grid-template-columns: repeat(2, 1fr);
+        .cards {
+            grid-template-columns: repeat(3, 1fr);
         }
     }
     
