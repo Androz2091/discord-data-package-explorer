8f2a7f841b12a9706f1dae4ae878adf6ebbd3a4b
Androz2091 <androz2091@gmail.com>
2021-02-20 09:58:26 +0100

    :sparkles: Add demo

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,39 @@
 
 import { writable } from 'svelte/store';
 
-export const loaded = writable(false);
-export const data = writable(null);
+let loadedValue = false;
+let dataValue = null;
+const isDemo = window.location.href.includes('demo');
+if (isDemo) {
+    loadedValue = true;
+    const demoUserObject = {
+        id: 422820341791064085,
+        username: 'Wumpus',
+        discriminator: '0000',
+        avatar: null
+    };
+    const demoMessageObject = {
+        id: 422820341791064085,
+        timestamp: 1613810737577
+    };
+    dataValue = {
+        user: demoUserObject,
+        channels: [
+            {
+                messages: [demoMessageObject]
+            }
+        ],
+        applications: [],
+        topDMs: [
+            {
+                userData: demoUserObject,
+                messages: new Array(2000).fill(demoMessageObject)
+            }
+        ],
+        messageCount: 0,
+        averageMessageCountPerDay: 0
+    };
+}
+
+export const loaded = writable(loadedValue);
+export const data = writable(dataValue);
