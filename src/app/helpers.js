db207a00f55249223b628ae44a246b63344fdc84
vanishedvan <vanished.apps@gmail.com>
2021-02-28 16:53:24 +0100

    Updated the mostOccurences function.

--- src/app/helpers.js
+++ src/app/helpers.js
@@ -10,17 +10,42 @@ export const getCreatedTimestamp = (id) => {
     return id / 4194304 + EPOCH;
 };
 
-export const mostOccurences = (arr) => {
-    const counts = {};
-    let maxCount = 0;
-    let maxKey;
-    for (let i = 0; i < arr.length; i++) {
-        const key = arr[i];
-        const count = (counts[key] = (counts[key] || 0) + 1);
-        if (count > maxCount) {
-            maxCount = count;
-            maxKey = key;
+export const mostOccurences = (arr, words = false) => {
+    if (!words) {
+        const counts = {};
+        let maxCount = 0;
+        let maxKey;
+
+        for (let i = 0; i < arr.length; i++) {
+            const key = arr[i];
+            const count = (counts[key] = (counts[key] || 0) + 1);
+            if (count > maxCount) {
+                maxCount = count;
+                maxKey = key;
+            }
         }
+
+        return maxKey;
+    } else {
+        arr = arr.flat(3);
+    
+        var item,
+            length = arr.length,
+            array = [],
+            object = {};
+    
+        for (var index = 0; index < length; index++) {
+            item = arr[index];
+            if (!item) continue;
+    
+            if (!object[item]) object[item] = 1;
+            else ++object[item];
+        }
+    
+        for(var p in object) array[array.length] = p;
+    
+        return array.sort((a, b) => {
+            return object[b] - object[a];
+        });
     }
-    return maxKey;
 };
