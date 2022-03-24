233ca2c5fe99100bff488e76066fd050ce2abeaa
Le_Roux-Nard <guymaxyt@gmail.com>
2022-03-24 08:22:34 -0400

    update demo to add dmsVoctime default values

--- src/app/demo.js
+++ src/app/demo.js
@@ -6,7 +6,7 @@ const demoUserObject = {
     discriminator: '0000',
     avatar: null
 };
-    
+
 export default () => {
 
     const removeAnalytics = window.location.href.includes('noanalytics');
@@ -70,13 +70,24 @@ export default () => {
             idGuild8: { name: 'Server n°8', voctime: '86400000' },
             idGuild9: { name: 'Server n°9', voctime: '43200000' },
             idGuild10: { name: 'Server n°10', voctime: '14400000' },
+        }, dmsVoctime: {
+            idGuild1: { name: 'User/Group n°1', voctime: '31536000000' },
+            idGuild2: { name: 'User/Group n°2', voctime: '16070400000' },
+            idGuild3: { name: 'User/Group n°3', voctime: '2629743000' },
+            idGuild4: { name: 'User/Group n°4', voctime: '1209600000' },
+            idGuild5: { name: 'User/Group n°5', voctime: '604800000' },
+            idGuild6: { name: 'User/Group n°6', voctime: '345600000' },
+            idGuild7: { name: 'User/Group n°7', voctime: '172800000' },
+            idGuild8: { name: 'User/Group n°8', voctime: '86400000' },
+            idGuild9: { name: 'User/Group n°9', voctime: '43200000' },
+            idGuild10: { name: 'User/Group n°10', voctime: '14400000' },
         },
 
         ...(!removeAnalytics && {
             openCount: randomNumber(200, 300),
             averageOpenCountPerDay: randomNumber(3, 5),
             notificationCount: randomNumber(200, 400),
-            joinVoiceChannelCount: randomNumber(40, 100), 
+            joinVoiceChannelCount: randomNumber(40, 100),
             joinCallCount: randomNumber(20, 30),
             addReactionCount: randomNumber(100, 200),
             messageEditedCount: randomNumber(50, 70),
